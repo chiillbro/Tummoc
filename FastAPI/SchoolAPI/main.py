61a0596b7f9c087669debfd481a5679e7f165fbd
Siva Sankar Reddy Asam <144758027+chiillbro@users.noreply.github.com>
2024-07-24 11:21:57 +0530

    Update main.py

--- FastAPI/SchoolAPI/main.py
+++ FastAPI/SchoolAPI/main.py
@@ -18,8 +18,17 @@ def get_db():
 class TeacherCreate(BaseModel):
     name: str
     subject: str
+    
+class TeacherUpdate(BaseModel):
+    name: str
+    subject: str
 
 class StudentCreate(BaseModel):
+    name: str
+    age: int
+    teacher_id: int
+
+class StudentUpdate(BaseModel):
     name: str
     age: int
     teacher_id: int
@@ -46,6 +55,28 @@ def create_teacher(teacher: TeacherCreate, db: Session = Depends(get_db)):
 def read_teachers(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     teachers = db.query(Teacher).offset(skip).limit(limit).all()
     return teachers
+    
+# update teacher
+@app.put("/teachers/{teacher_id}")
+def update_teacher(teacher_id: int,  teacher_update: TeacherUpdate, db: Session = Depends(get_db)):
+    teacher = db.query(Teacher).filter(Teacher.id == teacher_id).first()
+    if not teacher:
+        raise HTTPException(status_code=404, detail="Teacher not found")
+    for key, value in teacher_update.dict().items():
+        setattr(teacher, key, value)
+    db.commit()
+    db.refresh(teacher)
+    return teacher
+
+#delete teacher
+@app.delete("/teachers/{teacher_id}")
+def delete_teacher(teacher_id: int, db: Session = Depends(get_db)):
+    teacher = db.query(Teacher).filter(Teacher.id == teacher_id).first()
+    if not teacher:
+        raise HTTPException(status_code=404, detail="Teacher not found")
+    db.delete(teacher)
+    db.commit()
+    return {"message" : "Teacher deleted"}
 
 # create student
 @app.post("/students/")
@@ -61,6 +92,28 @@ def create_student(student: StudentCreate, db: Session = Depends(get_db)):
 def read_students(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     students = db.query(Student).offset(skip).limit(limit).all()
     return students
+
+# update student
+@app.put("/students/{student_id}")
+def update_student(student_id: int, student_update: StudentUpdate, db: Session = Depends(get_db)):
+    student = db.query(Student).filter(Student.id == student_id).first()
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    for key, value in student_update.dict().items():
+        setattr(student, key, value)
+    db.commit()
+    db.refresh(student)
+    return student
+
+# delete student
+@app.delete("/students/{student_id}")
+def delete_student(student_id: int, db: Session = Depends(get_db)):
+    student = db.query(Student).filter(Student.id == student_id).first()
+    if not student:
+        raise HTTPException(status_code=404, detail="Student not found")
+    db.delete(student)
+    db.commit()
+    return {"message" : "Student deleted"}
 
 # assign student to teacher
 @app.post("/assign-student/")
